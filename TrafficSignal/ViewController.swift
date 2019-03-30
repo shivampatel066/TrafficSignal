9e2fe7451a72701cba13c9328c19b006e1961c3b
Shivam Patel <shivampatel066@gmail.com>
2019-03-30 10:23:01 +0530

    Initial Commit

---
+++ TrafficSignal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrafficSignal
+//
+//  Created by Shivam on 30/03/19.
+//  Copyright © 2019 Shivam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
